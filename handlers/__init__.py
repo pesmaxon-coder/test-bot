e07fccc88e43afa85de46245c0ae0401c1f9ac19
pesmaxon-coder <pesmaxon@gmail.com>
2026-03-25 18:49:53 +0500

    Update __init__.py

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,5 +1,6 @@
 from handlers.registration import router as reg_router
 from handlers.test import router as test_router
 from handlers.settings import router as settings_router
+from handlers.paid_channels import router as paid_router
 
-__all__ = ["reg_router", "test_router", "settings_router"]
+__all__ = ["reg_router", "test_router", "settings_router","paid_router"]
